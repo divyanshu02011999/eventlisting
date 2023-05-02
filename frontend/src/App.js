027928ea167e96464287a0ecad95fba8e7766550
m-triple-m <triplem656@gmail.com>
2023-05-02 19:50:49 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/main';
-import Home from './components/main/Home';
-import Login from './components/main/Login';
-import Signup from './components/main/Signup';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/main/Navbar';
-import ManageUser from './components/user/ManageUser';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/main";
+import Home from "./components/main/Home";
+import Login from "./components/main/Login";
+import Signup from "./components/main/Signup";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/main/Navbar";
+import ManageUser from "./components/user/ManageUser";
 
 function App() {
   return (
     <div>
- <BrowserRouter>
- <Navbar />
-    <Routes>
-   
-   
-   
-      
-      <Route path="/" element={ <Navigate to="/home" /> } />
-      <Route path='main' element={ <Main/> } />
-      <Route path='home' element={ <Home/> } />
-      <Route path='login' element={ <Login/> } />
-      <Route path='signup' element={ <Signup/> } />
-     <Route path='manageuser' element={ <ManageUser/> } />
-    </Routes></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main/home" />} />
+          <Route path="main" element={<Main />}>
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
